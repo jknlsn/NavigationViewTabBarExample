7368b6a53486930cd7ea6cbf82cadece49f10b17
Jake Nelson <j.n@live.com.au>
2021-12-12 21:14:36 +0000

    Initial Commit

---
+++ NavigationViewTabBarExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigationViewTabBarExample
+//
+//  Created by Jake Nelson on 12/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavigationViewTabBarExample/NavigationViewTabBarExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewTabBarExampleApp.swift
+//  NavigationViewTabBarExample
+//
+//  Created by Jake Nelson on 12/12/21.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewTabBarExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
